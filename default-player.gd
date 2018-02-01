8e34109c35c9be322604307d50b30110b77e82e6
Daniel Timko <dantimko1@gmail.com>
2018-01-31 21:40:05 -0500

    Move the player!

--- default-player.gd
+++ default-player.gd
@@ -1,37 +1,56 @@
 extends KinematicBody2D
 
-export var playerSpeed = 10
+export var playerSpeed = 500
 
-var moveDirection = Vector2(0, 0)
-
-signal player_is_moving;
+var moveDir = Vector2(0, 0)
 
 func _ready():
 	set_process_input(true)
 
 func _physics_process(delta):
-	print(moveDirection)
-	pass
+	var velocity = moveDir.normalized() * playerSpeed * delta;
+	position += velocity;
 	
 func _input(ev):
-	# Move up input
+	##
+	## All of this logic only works for keyboard input
+	##
+	#Up is pressed
 	if (ev.is_action_pressed("player_move_up")):
-		moveDirection += Vector2(0, 1)
+		#Set the direction
+		moveDir = Vector2(moveDir.x, -1)
+		
+	#Up is released
 	if (ev.is_action_released("player_move_up")):
-		moveDirection -= Vector2(0, 1)
-	# Move down input
-	if (ev.is_action_pressed("player_move_down")):
-		moveDirection += Vector2(0, -1)
-	if (ev.is_action_released("player_move_down")):
-		moveDirection -= Vector2(0, -1)
-	# Move left input
-	if (ev.is_action_pressed("player_move_left")):
-		moveDirection += Vector2(-1, 0)
-	if (ev.is_action_released("player_move_left")):
-		moveDirection -= Vector2(-1, 0)
+		#If down is currently pressed, move down
+		if (Input.is_action_pressed("player_move_down")):
+			moveDir = Vector2(moveDir.x, 1)
+		else:
+			moveDir = Vector2(moveDir.x, 0)
+			
 	# Move right input
 	if (ev.is_action_pressed("player_move_right")):
-		moveDirection += Vector2(1, 0)
+		moveDir = Vector2(1, moveDir.y)
 	if (ev.is_action_released("player_move_right")):
-		moveDirection -= Vector2(1, 0)
-	
+		if (Input.is_action_pressed("player_move_left")):
+			moveDir = Vector2(-1, moveDir.y)
+		else:
+			moveDir = Vector2(0, moveDir.y)
+		
+	# Move down input
+	if (ev.is_action_pressed("player_move_down")):
+		moveDir = Vector2(moveDir.x, 1)
+	if (ev.is_action_released("player_move_down")):
+		if (Input.is_action_pressed("player_move_up")):
+			moveDir = Vector2(moveDir.x, -1)
+		else:
+			moveDir = Vector2(moveDir.x, 0)
+			
+	# Move left input
+	if (ev.is_action_pressed("player_move_left")):
+		moveDir = Vector2(-1, moveDir.y)
+	if (ev.is_action_released("player_move_left")):
+		if (Input.is_action_pressed("player_move_right")):
+			moveDir = Vector2(1, moveDir.y)
+		else:
+			moveDir = Vector2(0, moveDir.y)
