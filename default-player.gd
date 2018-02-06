4646c35200ef87c24e26ec9a1dc0d55aebe4a4c1
Daniel Timko (RIT Student) <det2948@ad.rit.edu>
2018-02-06 12:12:41 -0500

    Added player character animatin

--- default-player.gd
+++ default-player.gd
@@ -3,13 +3,16 @@ extends KinematicBody2D
 export var playerSpeed = 500
 
 var moveDir = Vector2(0, 0)
+var lastMoveDir = 2
 
 func _ready():
 	set_process_input(true)
+	$CollisionShape2D.visible = true
+	$AnimatedSprite.play("idle-south")
 
 func _physics_process(delta):
 	var velocity = moveDir.normalized() * playerSpeed * delta;
-	position += velocity;
+	move_and_collide(velocity)
 	
 func _input(ev):
 	##
@@ -54,3 +57,37 @@ func _input(ev):
 			moveDir = Vector2(1, moveDir.y)
 		else:
 			moveDir = Vector2(0, moveDir.y)
+	
+	#Finally, change animation based on the move direction vector
+	moveDir = moveDir.normalized()
+	
+	var spr = $AnimatedSprite
+	var sprFrames = spr.frames
+	#check for stopped
+	if (moveDir.length_squared() == 0):
+		if (lastMoveDir == 0):
+			spr.play("idle-north")
+		elif (lastMoveDir == 1):
+			spr.play("idle-east")
+		elif (lastMoveDir == 2):
+			spr.play("idle-south")
+		elif (lastMoveDir == 3):
+			spr.play("idle-west")
+		return
+	#check for north/south animation
+	if (abs(moveDir.x) < 0.7):
+		if (moveDir.y < 0):
+			lastMoveDir = 0
+			spr.play("run-north")
+		elif (moveDir.y > 0):
+			lastMoveDir = 2
+			spr.play("run-south")
+	#check for east/west animation
+	elif (abs(moveDir.y) < 0.8):
+		if (moveDir.x < 0):
+			lastMoveDir = 3
+			spr.play("run-west")
+		elif (moveDir.x > 0):
+			lastMoveDir = 1
+			spr.play("run-east")
+
--- tilesets/template.tsx
+++ tilesets/template.tsx
@@ -4,17 +4,89 @@
  <terraintypes>
   <terrain name="mines" tile="9"/>
  </terraintypes>
- <tile id="0" terrain=",,,0"/>
- <tile id="1" terrain=",,0,0"/>
- <tile id="2" terrain=",,0,"/>
- <tile id="3" terrain="0,0,0,"/>
- <tile id="4" terrain="0,0,,0"/>
- <tile id="8" terrain=",0,,0"/>
+ <tile id="0" terrain=",,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 0,64 16,64 16,16 64,16 64,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 0,16 64,16 64,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" terrain=",,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 64,0 64,64 48,64 48,16 0,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain="0,0,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="48" y="64">
+    <polyline points="0,0 0,-16 16,-16 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain="0,0,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="48">
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 0,64 16,64 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="9" terrain="0,0,0,0"/>
- <tile id="10" terrain="0,,0,"/>
- <tile id="11" terrain="0,,0,0"/>
- <tile id="12" terrain=",0,0,0"/>
- <tile id="16" terrain=",0,,"/>
- <tile id="17" terrain="0,0,,"/>
- <tile id="18" terrain="0,,,"/>
+ <tile id="10" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="48" y="0">
+    <polyline points="0,0 0,64 16,64 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11" terrain="0,,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="48" y="0">
+    <polyline points="0,0 0,16 16,16 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12" terrain=",0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polyline points="0,0 16,0 16,-16 0,-16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polyline points="0,0 0,64 64,64 64,48 16,48 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17" terrain="0,0,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="48">
+    <polyline points="0,0 64,0 64,16 0,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18" terrain="0,,,">
+  <objectgroup draworder="index">
+   <object id="1" x="48" y="0">
+    <polyline points="0,0 0,48 -48,48 -48,64 16,64 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
